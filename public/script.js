e96849064e6ba3e5e3a6c4d4eb3bac4b3189a975
U5 <kyutech.joko@gmail.com>
2025-03-09 23:12:33 +0900

    add:音声認識

--- public/script.js
+++ public/script.js
@@ -11,3 +11,31 @@ const incrementCounter = () => {
 const decrementCounter = () => {
   socket.emit("decrement");
 };
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = "ja-JP";
+recognition.continuous = true;
+
+recognition.onresult = (event) => {
+  const transcript = event.results[event.resultIndex][0].transcript.trim();
+  console.log("Recognized: ", transcript);
+
+  if (transcript.includes("東工大")) {
+    incrementCounter();
+  } else if (transcript.includes("科学大")) {
+    decrementCounter();
+  }
+};
+
+recognition.onerror = (event) => {
+  console.error("Speech Recognition error: ", event.error);
+};
+
+recognition.onend = () => {
+  console.log("Speech Recognition ended");
+  recognition.start();
+};
+
+recognition.start();
